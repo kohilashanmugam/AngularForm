89bd85e6d23aefda51061120d848b6d9cc6e5b0b
kohila <kohilapraha@gmail.com>
2022-06-14 09:09:18 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myfirsttdf';
+
+  countryList=[
+    {"countryname":"India"},
+    {"countryname":"USA"},
+    {"countryname":"England"}
+  ];
+  onSubmit(myForm:NgForm){
+    console.log(myForm);
+  }
 }
